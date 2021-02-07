c50a34614e9b968b5e55207ebe6d7335b6ddfe43
Polina-mind <popova.v.polina@gmail.com>
2021-02-08 00:43:16 +0200

    update

--- js/index.js
+++ js/index.js
@@ -36,7 +36,7 @@ function onStartClick() {
           "beforeend",
           `<img
             class="preloader"
-            src="../race-horse.svg"
+            src="../images/race-horse.svg"
             alt="horse"
             width="60"
             height="60"
